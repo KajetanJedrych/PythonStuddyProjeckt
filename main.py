ecd8bd8c5fb80f3c2a6c34a12837c1bd7628af31
kubeczek <jedrychk@gmail.com>
2021-06-08 18:50:29 +0200

    fixed issue with module

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from moduly import sniadanie
+
+
 print("Jaki posiłek chcesz zjeść?\n")
 print("1-Śniadanie\n2-Obiad\n3-Kolacje\n")
 
 wybor = input("Wybierz liczbę od 1 do 3: ")
 wyborInt = int(wybor)
 if wyborInt == 1 :
-    print("Jest ok")
-    from moduly import  sniadanie 
+  print("Co byś chciał zjesc na śniadanie?\n")
+  print("1-Jecznica z trzech jajek na maśle (252kcl)\n2-Omlet z 2 jajek i z bananem (320kcl)\n3-Bułka przenna z masłem i wędliną (280kcl )\n")
+  wybor1 = input("Wybierz liczbę od 1 do 3: ")
+  wyborSniadania = int(wybor1)
+  if wyborSniadania == 1:
+      print("wybrales jajecznice która ma 252 Kalorii zostało to zapisane w twoim menue!")
+      x = sniadanie.sniadanie1(1)
+      print(x)
 elif wyborInt == 2:  
     print("Obiad") 
 elif wyborInt == 3:
--- moduly/sniadanie.py
+++ moduly/sniadanie.py
@@ -1,8 +1,13 @@
-print("Co byś chciał zjesc na śniadanie?\n")
-print("1-Jecznica z trzech jajek na maśle (252kcl)\n2-Omlet z 2 jajek i z bananem (320kcl)\n3-Bułka przenna z masłem i wędliną (280kcl )\n")
-wybor = input("Wybierz liczbę od 1 do 3: ")
-wyborInt = int(wybor)
-if wyborInt == 1 or wyborInt == 2 or wyborInt == 3:
-    print("Jest ok")
-else:
-     print("Podaj właściwą wartość!")
+
+def sniadanie1(n):
+    if n == 1:
+        x= 252
+        return x
+    elif n ==2:
+        print("sss")
+        return 320
+    elif n ==3:
+        print("Asda")
+        return 666
+    else:
+        return print("podaj właściwa wartość!")
