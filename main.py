0c8d6d28a16bb74b0d5465de646bc4ab41247bd1
kubeczek <jedrychk@gmail.com>
2021-06-14 15:33:39 +0200

    add some elements to dictionary and play with it

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from moduly import sniadanie
 from moduly import obiad
 from moduly import kolacja
 menue = {}
-posilki = []
-licznik = 0
-while wyborInt != 4: 
+posilki = [""]
+end = 0
+licznik=1
+while end != 1: 
   print("Jaki posiłek chcesz zjeść?\n")
   print("1-Śniadanie\n2-Obiad\n3-Kolacje\n")
   print("4-Zakończ program")
-
+  menue[licznik]=(posilki[-1])
   wybor = input("Wybierz liczbę od 1 do 4: ")
   wyborInt = int(wybor)
   if wyborInt == 1 :
     print("Co byś chciał zjesc na śniadanie?\n")
     print("1-Jecznica z trzech jajek na maśle (252kcl)\n2-Omlet z 2 jajek i z bananem (320kcl)\n3-Bułka przenna z masłem i wędliną (280kcl )\n")
     wybor1 = input("Wybierz liczbę od 1 do 3: ")
+    licznik =+ 1
     wyborSniadania = int(wybor1)
     if wyborSniadania == 1:
       print("wybrales jajecznice która ma 252kcl zostało to zapisane w twoim menue!")
@@ -34,6 +36,7 @@ while wyborInt != 4:
      print("1-Pieczona pierś z Kurczaka z ryżem (520kcl)\n2-Makaron ryżowy z wieprzowiną w sosie słodko kwaśnym (640kcl)")
      print("3-Mc-royoal+Frytki+kola(1134kcl )\n")
      wybor2 = input("Wybierz liczbę od 1 do 3: ")
+     licznik =+ 1
      wyborObiad = int(wybor2)
      if wyborObiad == 1:
       print("wybrales pierś z kurczaka z ryżem która ma 2520 Kalorii zostało to zapisane w twoim menue!")
@@ -52,21 +55,23 @@ while wyborInt != 4:
     print("1-serek wiejski z rzodkiewką i kromką chleba przennego (265kcl)\n2-Twaróg na słodko z miodem i orzechami(540kcl)")
     print("3-Kebab z lokalnej kebabowni(670kcl )\n")
     wybor3 = input("Wybierz liczbę od 1 do 3: ")
+    licznik =+ 1
     wyborKolacja = int(wybor3)
     if wyborKolacja == 1:
       print("wybrales serek wiejski z rzodkiewką i kromką chleba który ma 265kcl zostało to zapisane w twoim menue!")
-      serek = kolacja.kolacja(1)
+      serek = kolacja.kolacja1(1)
       posilki.append("serek wiejski z rzodkiewką i kromką chleba przennego (265kcl)")
     elif wyborKolacja ==2:
        print("Wybrales Twaróg z miodem i orzechami który ma 540kcl zostało to zapisane w twoim menue!")
-       twarog = kolacja.kolacja(2)
+       twarog = kolacja.kolacja1(2)
        posilki.append("Twaróg na słodko z miodem i orzechami(540kcl)")
     elif wyborKolacja ==3:
         print("Wybrales Kebaba 670kcl zostało to zapisane w twoim menue!")
-        kebab = kolacja.kolacja(3) 
+        kebab = kolacja.kolacja1(3) 
         posilki.append("Kebab z lokalnej kebabowni(670kcl )")
   elif wyborInt ==4:
-    licznik =4          
+    print(menue) 
+    end = 1        
   else:
      print("Podaj właściwą wartość!")
 
